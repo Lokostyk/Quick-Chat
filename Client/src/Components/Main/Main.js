a2b3e77d2ef692d31e19740e2376890fdd118960
Lokostyk <lokostyk12@gmail.com>
2022-01-14 22:45:26 +0100

    Added front page

--- Client/src/Components/Main/Main.js
+++ Client/src/Components/Main/Main.js
@@ -1,11 +1,15 @@
+import "./main.scss"
 
 function Main() {
     return (
-    <section>
-      <div>
-
+    <section className="mainContainer">
+      <div className="subContainer">
+        <h1>Quick Chat</h1>
+        <p></p>
+        <button className="loginBtns">Login</button>
+        <button className="loginBtns">Register</button>
       </div>
-      <img />
+      <img className="backgroundImage" src="/Images/social.svg" />
     </section>
     )
 }
